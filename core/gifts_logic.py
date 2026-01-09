b2e53c46aca7597eb00b01380da70ecca297f15d
Emmy R <emmykristinaroll@gmail.com>
2026-01-09 12:29:38 +0100

    stage changes for commit

--- core/gifts_logic.py
+++ core/gifts_logic.py
@@ -2,11 +2,16 @@ import random                # "random" genererar ett slumpmässigt förslag.
 from core.gifts_data import GIFTS # Importerar dictionary'n "GIFTS" från filen "gifts_data".
 
 # Skapar en funktion som tar emot en kategori, t.ex "man".
-def get_random_gift(category: str) -> str | None: # får in en variabel som heter "category", som ska vara text (str).
-    gifts = GIFTS.get(category, []) # hämtar listan med juklappar för vald kategori.
-    if not gifts:                   # om kategorin inte finns returneras en tom lista istället för att krascha.
-        return None                 # om listan är tom finns inga förslag, returnerar då None.
-    return random.choice(gifts)     # väljer ett slumpmässigt objekt i listan och skickar tilbaka det till main.
+def get_random_gift(category: str, used_gifts: set[str]) -> str | None:
+    all_gifts = GIFTS.get(category, [])
+
+    # filtrera bort presenter som redan använts
+    available_gifts = [gift for gift in all_gifts if gift not in used_gifts]
+
+    if not available_gifts:
+        return None
+    
+    return random.choice(available_gifts)
 
 '''
 en funktion som tar emot vilken kategori du valt och sedan skickar tillbaka en julklappsidé,
